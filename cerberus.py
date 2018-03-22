93d80282159c728e6209ec698ed55f824a991917
abricker <abricker@wustl.edu>
2018-03-22 11:26:04 -0500

    Added better syntax, role support, etc

--- cerberus.py
+++ cerberus.py
@@ -11,6 +11,7 @@ with open('config.json') as config_file:
 discordKey = str(cfg['discord'])
 validInputChannels = cfg['channels']['listening']
 logChannel = str(cfg['channels']['log'])
+validRoles = cfg['roles']
 
 client = discord.Client()
 
@@ -20,17 +21,28 @@ sqlconn.execute("CREATE TABLE IF NOT EXISTS badeggs (dbid, INT PRIMARY KEY, id I
 sqlconn.commit()
 sqlconn.close()
 
+warnThreshold = 2
+
 def removeCommand(m):
     tmp = m.split(" ")[2:]
     return " ".join(tmp)
 
 def formatTime(t):
     # Input t is of the form: YYYY-MM-DD HH:MM:SS.SSSSSS
-    date = t.split(" ")[0]
+    date = str(t).split(" ")[0]
     pieces = date.split("-")
     # output is of the form DD/MM/YYYY
     european = "{}/{}/{}".format(pieces[2], pieces[1], pieces[0])
     return european
+
+def checkRoles(user):
+    if len(validRoles) == 1 and validRoles[0] == "":
+        return True
+    for role in user.roles:
+        for r in validRoles:
+            if role.id == r:
+                return True
+    return False
 
 async def userSearch(u, m):
     if (len(u) == 1):
@@ -45,14 +57,43 @@ async def userSearch(u, m):
             out = "That user was found with the following infractions:\n"
             for item in searchResults:
                 if item[1] == 0:
-                    out += "{} was banned on {} by {} for this reason: {}\n".format(item[0], formatTime(item[2]), item[4], item[3])
+                    out += "[{}] **{}** - Banned by {} - {}\n".format(formatTime(item[2]), item[0], item[4], item[3])
                 else:
-                    out += "{} was received Warning {} on {} by {} for this reason: {}\n".format(item[0], item[1], formatTime(item[2]), item[4], item[3])
+                    out += "[{}] **{}** - Warning #{} by {} - {}\n".format(formatTime(item[2]), item[0], item[1], item[4], item[3])
                 if item[1] == 3:
-                    out += "They have received 3 warning, it is recommended that they be banned.\n"
+                    out += "They have received {} warnings, it is recommended that they be banned.\n".format(warnThreshold)
             await client.send_message(m.channel, out)
     else:
         await client.send_message(m.channel, "Please mention only a single user that you wish to search")
+
+async def logUser(u, m, ban):
+    if len(u) == 1:
+        sqlconn = sqlite3.connect('sdv.db')
+        # TODO: This could potentially be a problem if you try to warn again after banning
+        if (ban):
+            count = 0
+        else:
+            count = sqlconn.execute("SELECT COUNT(*) FROM badeggs WHERE id=?", [u[0].id]).fetchone()[0] + 1
+        globalcount = sqlconn.execute("SELECT COUNT(*) FROM badeggs").fetchone()[0]
+        currentTime = datetime.datetime.utcnow()
+        params = [globalcount + 1, u[0].id, "{}#{}".format(u[0].name, u[0].discriminator), count, currentTime, removeCommand(m.content), m.author.name]
+        sqlconn.execute("INSERT INTO badeggs (dbid, id, username, num, date, message, staff) VALUES (?, ?, ?, ?, ?, ?, ?)", params)
+        sqlconn.commit()
+        sqlconn.close()
+        if ban:
+            logMessage = "[{}] **{}#{}** - Banned by {} - {}\n".format(formatTime(currentTime), u[0].name, u[0].discriminator, m.author.name, removeCommand(m.content))
+        else:
+            logMessage = "[{}] **{}#{}** - Warning #{} by {} - {}\n".format(formatTime(currentTime), u[0].name, u[0].discriminator, count, m.author.name, removeCommand(m.content))
+        try:
+            await client.send_message(client.get_channel(logChannel), logMessage)
+        except discord.errors.InvalidArgument:
+            await client.send_message(m.channel, "The logging channel has not been set up in `config.json`. In order to have a visual record, please specify a channel ID.")
+
+        if (count > warnThreshold and ban == False):
+            logMessage += "This user has received {} warnings or more. It is recommened that they be banned.".format(warnThreshold)
+        await client.send_message(m.channel, logMessage)
+    else:
+        await client.send_message(m.channel, "Only mention the user you wish to log.")
 
 @client.event
 async def on_ready():
@@ -66,30 +107,17 @@ async def on_message(message):
         try:
             if message.channel.id in validInputChannels:
                 if message.content.startswith("!search"):
-                    user = message.mentions
-                    await userSearch(user, message)
-                elif message.content.startswith("!warned"):
-                    user = message.mentions
-                    if len(user) == 1:
-                        sqlconn = sqlite3.connect('sdv.db')
-                        # TODO: This could potentially be a problem if you try to warn again after banning
-                        count = sqlconn.execute("SELECT COUNT(*) FROM badeggs WHERE id=?", [user[0].id]).fetchone()[0]
-                        globalcount = sqlconn.execute("SELECT COUNT(*) FROM badeggs").fetchone()[0]
-                        currentTime = datetime.datetime.utcnow()
-                        params = [globalcount + 1, user[0].id, user[0].name, count + 1, currentTime, removeCommand(message.content), message.author.name]
-                        sqlconn.execute("INSERT INTO badeggs (dbid, id, username, num, date, message, staff) VALUES (?, ?, ?, ?, ?, ?, ?)", params)
-                        sqlconn.commit()
-                        sqlconn.close()
-                        await client.send_message(message.channel, "User was warned.")
-                        logMessage = "{} (ID: {}) received Warning #{} by {} for the following reason:\n{}".format(user[0].name, user[0].id, count+1, message.author.name, removeCommand(message.content))
-                        if (count > 2):
-                            logMessage += "This user has received 3 warnings or more. It is recommened that they be banned."
-                        await client.send_message(client.get_channel(logChannel), logMessage)
+                    if checkRoles(message.author):
+                        user = message.mentions
+                        await userSearch(user, message)
+                elif message.content.startswith("!warn"):
+                    if checkRoles(message.author):
+                        user = message.mentions
+                        await logUser(user, message, False)
                 elif message.content.startswith("!banned"):
-                    print("Banned User")
-            else:
-                print("Invalid channel")
-
+                    if checkRoles(message.author):
+                        user = message.mentions
+                        await logUser(user, message, True)
         except discord.errors.HTTPException:
             pass
 

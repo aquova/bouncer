faf1b9d2c984e9db699055fc8f87c4d7fc7754f8
abricker <abricker@wustl.edu>
2018-07-24 18:55:46 -0500

    Redid help and main function, fixed rewrite bugs

--- bouncer.py
+++ bouncer.py
@@ -1,12 +1,16 @@
 """
-A Discord moderation bot, originally made for the Stardew Valley server
+Bouncer
 Written by aquova, 2018
 https://github.com/aquova/bouncer
 """
 
 import discord, json, sqlite3, datetime, asyncio, os
+from discord.ext import commands
 import Utils
 from Member import User
+
+description = "A Discord moderation bot, originally made for the Stardew Valley server"
+bot = commands.Bot(command_prefix="$", description=description)
 
 # Reading values from config file
 with open('config.json') as config_file:
@@ -18,8 +22,8 @@ validInputChannels = cfg['channels']['listening']
 logChannel = str(cfg['channels']['log'])
 validRoles = cfg['roles']
 
-sendBanDM = (cfg['DM']['ban'].upper() == "TRUE")
-sendWarnDM = (cfg['DM']['warn'].upper() == "TRUE")
+sendBanDM = (cfg['DM']['ban'].upper() == "ON")
+sendWarnDM = (cfg['DM']['warn'].upper() == "ON")
 
 client = discord.Client()
 
@@ -38,6 +42,16 @@ checkCooldown = datetime.timedelta(minutes=5)
 # Containers to store needed information in memory
 recentBans = {}
 blockList = []
+
+helpInfo = {'$WARN':       '`$warn USER reason`',
+            '$BAN':        '`$ban USER reason`',
+            '$SEARCH':     '`$search USER`',
+            '$NOTE':       '`$note USER message',
+            '$REMOVE':     '`$remove USER',
+            '$NOTEREMOVE': '`$noteremove USER`',
+            '$BLOCK':      '`$block USER`',
+            '$UNBLOCK':    '`$unblock USER',
+            '$REPLY':      '`$reply USER`'}
 
 # This is basically a makeshift enum
 class LogTypes:
@@ -189,32 +203,34 @@ async def removeError(m, state):
 # Prevents DM from a specific user from being forwarded
 # message: Discord message object
 # block: Boolean, true for block, false for unblock
-async def blockUser(message, block):
+async def blockUser(m, block):
     global blockList
-    uid = Utils.getID(message)
-    if uid == None:
-        await client.send_message(message.channel, "That was not a valid user ID")
+    try:
+        user = User(m, recentBans)
+    except User.MessageError:
+        await client.send_message(m.channel, "I wasn't able to understand that message: `$block USER`")
         return
+
     sqlconn = sqlite3.connect('sdv.db')
     if block:
-        if uid in blockList:
-            await client.send_message(message.channel, "Um... That user was already blocked...")
+        if user.id in blockList:
+            await client.send_message(m.channel, "Um... That user was already blocked...")
         else:
-            sqlconn.execute("INSERT INTO blocks (id) VALUES (?)", [uid])
-            blockList.append(uid)
-            await client.send_message(message.channel, "I have now blocked {}. Their messages will no longer display in chat, but they will be logged for later review.".format(uid))
+            sqlconn.execute("INSERT INTO blocks (id) VALUES (?)", [user.id])
+            blockList.append(user.id)
+            await client.send_message(m.channel, "I have now blocked {}. Their messages will no longer display in chat, but they will be logged for later review.".format(user.id))
     else:
-        if uid not in blockList:
-            await client.send_message(message.channel, "That user hasn't been blocked...")
+        if user.id not in blockList:
+            await client.send_message(m.channel, "That user hasn't been blocked...")
         else:
-            sqlconn.execute("DELETE FROM blocks WHERE id=?", [uid])
-            blockList.remove(uid)
-            await client.send_message(message.channel, "I have now unblocked {}. You will once again be able to hear their dumb bullshit in chat.".format(uid))
+            sqlconn.execute("DELETE FROM blocks WHERE id=?", [user.id])
+            blockList.remove(user.id)
+            await client.send_message(m.channel, "I have now unblocked {}. You will once again be able to hear their dumb bullshit in chat.".format(user.id))
     sqlconn.commit()
     sqlconn.close()
 
 # Sends a private message to the specified user
-async def reply(message):
+async def reply(m):
     try:
         user = User(m, recentBans)
     except User.MessageError:
@@ -223,21 +239,21 @@ async def reply(message):
 
     u = user.getMember()
     if u == None:
-        await client.send_message(message.channel, "Sorry, but they need to be in the server for me to message them")
+        await client.send_message(m.channel, "Sorry, but they need to be in the server for me to message them")
         return
     try:
-        await client.send_message(u, "A message from the SDV staff: {}".format(Utils.removeCommand(message.content)))
-        await client.send_message(message.channel, "Message sent.")
+        await client.send_message(u, "A message from the SDV staff: {}".format(Utils.removeCommand(m.content)))
+        await client.send_message(m.channel, "Message sent.")
 
     # I don't know if any of these are ever getting tripped
     except discord.errors.HTTPException as e:
-        await client.send_message(message.channel, "ERROR: While attempting to DM, there was an unexpected error. Tell aquova this: {}".format(e))
+        await client.send_message(m.channel, "ERROR: While attempting to DM, there was an unexpected error. Tell aquova this: {}".format(e))
     except discord.errors.Forbidden:
-        await client.send_message(message.channel, "ERROR: I am not allowed to DM the user. It is likely that they are not accepting DM's from me.")
+        await client.send_message(m.channel, "ERROR: I am not allowed to DM the user. It is likely that they are not accepting DM's from me.")
     except discord.errors.NotFound:
-        await client.send_message(message.channel, "ERROR: I was unable to find the user to DM. I'm unsure how this can be the case, unless their account was deleted")
+        await client.send_message(m.channel, "ERROR: I was unable to find the user to DM. I'm unsure how this can be the case, unless their account was deleted")
 
-async def notebook(message):
+async def notebook(m):
     sqlconn = sqlite3.connect('sdv.db')
     allNotes = sqlconn.execute("SELECT * FROM badeggs WHERE num=-1").fetchall()
     sqlconn.commit()
@@ -249,9 +265,9 @@ async def notebook(message):
         if len(note) + len(out) < 2000:
             out += note
         else:
-            await client.send_message(message.channel, out)
+            await client.send_message(m.channel, out)
             out = note
-    await client.send_message(message.channel, out)
+    await client.send_message(m.channel, out)
 
 # Special function made for SDV multiplayer beta release
 # If matching phrase is posted, then post link to bug submission thread
@@ -305,68 +321,41 @@ async def on_message(message):
                 with open("DMs.txt", 'a', encoding='utf-8') as openFile:
                     openFile.write(mes)
             else:
-                mes = "User {}#{} (ID: {}) has sent me a private message: {}".format(message.author.name, message.author.discriminator, message.author.id, message.content)
+                mes = "**{}#{}** (ID: {}): {}".format(message.author.name, message.author.discriminator, message.author.id, message.content)
                 if message.attachments != []:
                     for item in message.attachments:
                         mes += '\n' + item['url']
                 await client.send_message(client.get_channel(validInputChannels[0]), mes)
 
-        if (message.channel.id in validInputChannels) and Utils.checkRoles(message.author, validRoles):
-            if message.content.startswith("$dumpbans"):
-                output = Utils.dumpBans(recentBans)
-                await client.send_message(message.channel, output)
-            elif message.content.startswith("$search"):
-                if message.content == "$search":
-                    await client.send_message(message.channel, "`$search USER`")
-                else:
-                    await userSearch(message)
+        elif (message.channel.id in validInputChannels) and Utils.checkRoles(message.author, validRoles):
+            if len(message.content.split(" ")) == 1:
+                if message.content.upper() == "$HELP":
+                    helpMes = "Issue a warning: `$warn USER message`\nLog a ban: `$ban USER reason`\nSearch for a user: `$search USER`\nCreate a note about a user: `$note USER message`\nShow all notes: `$notebook`\nRemove a user's last log: `$remove USER`\nRemove a user's last note: `$noteremove USER`\nStop a user from sending DMs to us: `$block/$unblock USERID`\nReply to a user in DMs: `$reply USERID`\nDMing users when they are banned is `{}`\nDMing users when they are warned is `{}`".format(sendBanDM, sendWarnDM)
+                    await client.send_message(message.channel, helpMes)
+                elif message.content.upper() == "$NOTEBOOK":
+                    await notebook(message)
+                elif message.content.upper() in helpInfo.keys():
+                    await client.send_message(message.channel, helpInfo[message.content.upper()])
+                return
+
+            if message.content.startswith("$search"):
+                await userSearch(message)
             elif message.content.startswith("$warn"):
-                if message.content == "$warn":
-                    await client.send_message(message.channel, "`$warn USER reason`")
-                else:
-                    await logUser(message, LogTypes.WARN)
+                await logUser(message, LogTypes.WARN)
             elif message.content.startswith("$ban"):
-                if message.content == "$ban":
-                    await client.send_message(message.channel, "`$ban USER reason`")
-                else:
-                    await logUser(message, LogTypes.BAN)
+                await logUser(message, LogTypes.BAN)
             elif message.content.startswith("$remove"):
-                if message.content == "$remove":
-                    await client.send_message(message.channel, "`$remove USER`")
-                else:
-                    await removeError(message, LogTypes.BAN)
+                await removeError(message, LogTypes.BAN)
             elif message.content.startswith("$block"):
-                if message.content == "$block":
-                    await client.send_message(message.channel, "`$block USER`")
-                else:
-                    await blockUser(message, True)
+                await blockUser(message, True)
             elif message.content.startswith("$unblock"):
-                if message.content == "$unblock":
-                    await client.send_message(message.channel, "`$unblock USER`")
-                else:
-                    await blockUser(message, False)
-            elif message.content.split(" ")[0].upper() == "$REPLY":
-                if message.content == "$reply":
-                    await client.send_message(message.channel, "`$reply USERID message`")
-                else:
-                    await reply(message)
-            # Needs to come before $note
-            elif message.content.startswith("$notebook"):
-                await notebook(message)
-            # Also needs to come before $note
+                await blockUser(message, False)
+            elif message.content.startswith("$reply"):
+                await reply(message)
             elif message.content.startswith("$noteremove"):
-                if message.content == "$noteremove":
-                    await client.send_message(message.channel, "`$noteremove USER`")
-                else:
-                    await removeError(message, LogTypes.NOTE)
-            elif message.content.split(" ")[0].upper() == "$NOTE":
-                if message.content == "$note":
-                    await client.send_message(message.channel, "`$note USERID message`")
-                else:
-                    await logUser(message, LogTypes.NOTE)
-            elif message.content.startswith('$help'):
-                helpMes = "Issue a warning: `$warn USER message`\nLog a ban: `$ban USER reason`\nSearch for a user: `$search USER`\nCreate a note about a user: `$note USER message`\nShow all notes: `$notebook`\nRemove a user's last log: `$remove USER`\nRemove a user's last note: `$noteremove USER`\nStop a user from sending DMs to us: `$block/$unblock USERID`\nReply to a user in DMs: `$reply USERID`\nDMing users when they are banned is `{}`\nDMing users when they are warned is `{}`".format(sendBanDM, sendWarnDM)
-                await client.send_message(message.channel, helpMes)
+                await removeError(message, LogTypes.NOTE)
+            elif message.content.startswith("note"):
+                await logUser(message, LogTypes.NOTE)
 
         # A five minute cooldown for responding to people who mention bug reports
         if (datetime.datetime.utcnow() - lastCheck > checkCooldown):

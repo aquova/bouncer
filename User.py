0fda5f2dfbdc91619fc554701532850688d76650
aquova <abrick912@gmail.com>
2019-11-23 08:42:33 -0600

    Added comments to User.py

--- User.py
+++ User.py
@@ -2,57 +2,115 @@ import discord, sqlite3
 import Utils
 from Utils import DATABASE_PATH
 
+"""
+User Class
+
+A class that, given a Discord message, will attempt to identify the corresponding User
+"""
 class User:
+    """
+    Custom Error, thrown when message couldn't be parsed to find a user
+    """
     class MessageError(Exception):
         pass
 
+    """
+    Initialization
+
+    Inputs:
+        message: Discord message object
+        banList: Dictionary of users who have been banned/left the server since startup
+    """
     def __init__(self, message, banList):
         self.message = message
         self.id = self.getID(banList)
 
-    # Gets the ID from a message
-    def getID(self, banList):
-        # First check if they're mentioned
-        if len(self.message.mentions) > 0:
-            # Need to make sure pinged user is in the right position
-            possibleUsername = self.message.mentions[0].id
-            if (self.message.content.split(" ")[1] == "<@{}>".format(possibleUsername) or (self.message.content.split(" ")[1] == "<@!{}>".format(possibleUsername))):
-                return possibleUsername
+    """
+    Get ID
 
-        # Then check if username is valid
+    Attempts to get the Discord user ID for the user mentioned in the message
+
+    Input:
+        banList: Dictionary of users who have been banned/left the server since startup
+    """
+    def getID(self, banList):
+        # Users can be mentioned one of three ways:
+        # - Simply by their ID
+        # - Simply by their username
+        # - By pinging them
+
+        # Check if any user was pinged in the message
+        if len(self.message.mentions) > 0:
+            # The pinging should take place as the first word of the message (in case users want to ping someone else in the log body)
+            possibleID = self.message.mentions[0].id
+            # Discord pings are of the form '<@ID>' or '<@!ID>'. Need to check for both
+            if (self.message.content.split(" ")[1] == "<@{}>".format(possibleID) or (self.message.content.split(" ")[1] == "<@!{}>".format(possibleID))):
+                return possibleID
+
+        # Check if the username was provided
         checkUsername = Utils.parseUsername(self.message, banList)
         if checkUsername != None:
             return checkUsername
-        checkID = self.message.content.split(" ")[1]
 
         # Finally, check if it is an ID
+        checkID = self.message.content.split(" ")[1]
         try:
+            # Simply verify by attempting to cast to an int. If it doesn't raise an error, return it
+            # Lengths of Discord IDs seem to be no longer a constant length, so difficult to verify that way
             int(checkID)
             return checkID
         except ValueError:
             raise self.MessageError("Couldn't understand message.")
 
+    """
+    Get Member
+
+    Attempts to identify a Discord member object from self.id
+    """
     def getMember(self):
+        # Iterate through all Discord members, check if IDs are the same
+        # TODO: May want to throw error if self.id is invalid
         u = [x for x in self.message.guild.members if x.id == int(self.id)]
+
+        # ID's are unique, so can simply return the first match (if there is one)
         if len(u) == 0:
             return None
         else:
             return u[0]
 
+    """
+    Get Name
+
+    Attempts to get the user's name from their Discord User object
+
+    Inputs:
+        banList: Dictionary of users who have been banned/left the server since startup
+    """
     def getName(self, banList):
         member = self.getMember()
+        # If we found a member, simply format the username and return
         if member != None:
             return "{}#{}".format(member.name, member.discriminator)
+
+        # If their ID was found in the ban list, it's already formatted properly, so return it
         if self.id in banList:
             return banList[self.id]
+
+        # Otherwise, they aren't in the server, and haven't left since startup.
+        # Check the database to see if we can get their name from a previous infraction
         checkDatabase = self.search()
         if checkDatabase == []:
             raise self.MessageError("User not found")
         return checkDatabase[-1][0]
 
+    """
+    Search
+
+    Attempt to search the database for a user with ID of self.id
+    """
     def search(self):
         sqlconn = sqlite3.connect(DATABASE_PATH)
-        searchResults = sqlconn.execute("SELECT username, num, date, message, staff, dbid FROM badeggs WHERE id=?", [self.id]).fetchall()
+        searchResults = sqlconn.execute("SELECT username FROM badeggs WHERE id=?", [self.id]).fetchall()
         sqlconn.commit()
         sqlconn.close()
 

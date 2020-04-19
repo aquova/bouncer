d47c8b97120b986e2be506dcc091b5f51c63a4ae
aquova <abrick912@gmail.com>
2020-04-18 20:39:29 -0500

    Add filter message
    Adds a DM message to a user when their post gets caught by the censor.

--- src/censor.py
+++ src/censor.py
@@ -5,7 +5,7 @@ from re import search
 
 def check_censor(message):
     # If you're posting in an admin channel, you can swear all you like
-    if message.id in VALID_INPUT_CHANS:
+    if message.channel.id in VALID_INPUT_CHANS:
         return False
 
     for item in CENSOR_LIST:
@@ -19,6 +19,7 @@ async def censor_message(message):
     # - Delete message
     # - Post a message removal message ourselves (since bots are normally ignored)
     # - Point the admins to the fact that we deleted a message
+    # - DM the user telling them that we deleted their message
 
     await message.delete()
 
@@ -29,3 +30,10 @@ async def censor_message(message):
     mod_mes = "Uh oh, looks like the censor might've been tripped.\n{}".format(get_mes_link(log_message))
     chan = discord.utils.get(message.guild.channels, id=CENSOR_CHAN)
     await chan.send(mod_mes)
+
+    # Create a DM channel between Bouncer if it doesn't exist
+    dm_chan = message.author.dm_channel
+    if dm_chan == None:
+        dm_chan = await message.author.create_dm()
+
+    await dm_chan.send("Hi there! This is an automated courtesy message informing you that your post was deleted for containing a censored word: `{}`. This is not a warning. The staff team will examine the context and situation of the censor trip and you will be contacted later only if any disciplinary action is taken.".format(message.content))

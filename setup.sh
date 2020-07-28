98b7032fc9c9669683c6f1154976fc00ca70563a
aquova <abrick912@gmail.com>
2020-07-28 17:29:54 -0500

    Correct private directory location in setup script

--- setup.sh
+++ setup.sh
@@ -30,8 +30,8 @@ python3 -m pip install -r requirements.txt;
 
 # Make default configuration files
 echo -e "${BLUE}Generating default configuration files${NC}";
-mkdir -p src/private;
-cat << EOF > src/private/config.json
+mkdir -p private;
+cat << EOF > private/config.json
 {
     "discord": "YOUR DISCORD TOKEN HERE",
     "debug": "False",

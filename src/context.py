1469d7c9b212c425f66969af2169bf7501015082
aquova <abrick912@gmail.com>
2022-06-04 10:26:22 -0500

    Okay, no really only allow admins to use Search context command

--- src/context.py
+++ src/context.py
@@ -2,7 +2,8 @@ import discord
 from client import client
 import commands
 
-@client.tree.context_menu(name="Search", default_permissions=discord.Permissions.none())
+@client.tree.context_menu(name="Search")
+@discord.app_commands.default_permissions() # Only allow admins to use this command
 async def search_context(interaction: discord.Interaction, user: discord.Member):
     response = await commands.search_helper(user.id)
     await interaction.response.send_message(response, ephemeral=False)

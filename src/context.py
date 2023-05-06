3c6b8148730eb3e685a0b512ee1c8c30b54fbcf5
b-b-blueberry <dearbblueberry@gmail.com>
2023-05-06 14:53:47 +1000

    Update Report-command for finding existing reply threads.

--- src/context.py
+++ src/context.py
@@ -27,7 +27,6 @@ class ReportResolveButton(discord.ui.Button):
         view: discord.ui.View = self.view
         view.remove_item(self)
         await interaction.message.edit(embed=embed, view=view)
-
         await interaction.response.defer()
 
 
@@ -51,17 +50,23 @@ class ReportThreadButton(discord.ui.Button):
 
             view: discord.ui.View = self.view
             view.remove_item(self)
-            view.add_item(ReportThreadButton(thread_url=thread.jump_url))
+            view.add_item(ReportThreadButton(
+                reported_user=self.reported_user,
+                thread_url=thread.jump_url if thread else None)
+            )
             await interaction.message.edit(view=view)
-
             await interaction.response.defer()
 
 
 class ReportMailboxView(discord.ui.View):
-    def __init__(self):
+    def __init__(self, *, reported_user: discord.User):
         super().__init__(timeout=0)
 
-        self.thread_button = ReportThreadButton()
+        thread_id: int | None = message_forwarder.get_reply_thread_id_for_user(user=reported_user)
+        thread: discord.Thread | None = client.get_channel(thread_id) if thread_id else None
+        self.thread_button = ReportThreadButton(
+            reported_user=reported_user,
+            thread_url=thread.jump_url if thread else None)
         self.add_item(ReportResolveButton())
         self.add_item(self.thread_button)
 
@@ -86,6 +91,7 @@ class ReportModal(discord.ui.Modal):
             colour=discord.Colour.gold(),
             url=self.message.jump_url)
 
+        reported_user: discord.User = self.message.author
         message_str: str = combine_message(self.message)
         comments_str: str = self.comments_input.value
 
@@ -96,15 +102,15 @@ class ReportModal(discord.ui.Modal):
                 value=field[1] if len(field[1]) < len_max else f"{field[1][:len_max-1]}…",
                 inline=field[2])
             for field in [
-                ("Suspect", self.message.author.mention, True),
+                ("Suspect", reported_user, True),
                 ("Reported by", interaction.user.mention, True),
                 (f"Sent <t:{int(self.message.created_at.timestamp())}:R>:", message_str, False),
                 ("Comments:", comments_str, False)
             ] if field[1]
         ]
 
-        log_chan = interaction.guild.get_channel(MAILBOX)
-        await log_chan.send(embed=embed, view=ReportMailboxView())
+        log_chan = client.get_channel(MAILBOX)
+        await log_chan.send(embed=embed, view=ReportMailboxView(reported_user=reported_user))
         await interaction.response.send_message(
             content="Your report has been forwarded to the server staff. Thanks!",
             ephemeral=True)

18d22f897c1e66215005ff0ff1f661f14d6b50b3
aquova <mail@aquova.net>
2023-12-16 12:21:02 -0600

    Fix typo

--- src/context.py
+++ src/context.py
@@ -144,4 +144,4 @@ async def report_context(interaction: discord.Interaction, message: discord.Mess
 
 @client.tree.context_menu(name="Report Message")
 async def report_message_context(interaction: discord.Interaction, _: discord.Member):
-    await interaction.response.send_message(interaction, "If you want to report someone, you need to select the message, not the user.", ephemeral=True)
+    await interaction.response.send_message("If you want to report someone, you need to select the message, not the user.", ephemeral=True)
